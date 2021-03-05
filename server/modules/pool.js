d3408212dd09e679646c7d39db14c84fabb5a9bb
Duncan Phelps <phelps.duncan@gmail.com>
2021-03-05 14:01:17 -0600

    trying to get db to connect

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -14,20 +14,9 @@ const url = require('url');
 let config = {};
 
 if (process.env.DATABASE_URL) {
-  // Heroku gives a url, not a connection object
-  // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
-
   config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true, // heroku requires ssl to be true
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   };
 } else {
   config = {
